e79db9a919b667f6eb28b3a9712843fcb6b3242e
AlexShi <alexshihov55@gmail.com>
2021-10-18 00:23:54 +0300

    Initial commit

---
+++ HomeworkGeekbrains/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HomeworkGeekbrains
+//
+//  Created by Александр on 18.10.2021.
+//  Copyright © 2021 Александр. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
