27331db2e83f11a5c1fcbb3c3b6892a47fde84fa
AlexShi <alexshihov55@gmail.com>
2021-11-03 17:01:09 +0300

    Done with lesson six

--- HomeworkGeekbrains/main.swift
+++ HomeworkGeekbrains/main.swift
@@ -8,5 +8,58 @@
 
 import Foundation
 
-print("Hello, World!")
+struct Queue<T> {
+    var people = [T]()
+    var count: Int {
+        return people.count
+    }
+  
 
+    mutating func addMan(name: T){
+        people.append(name)
+    }
+    mutating func addPeople(names: [T]){
+        people.append(contentsOf: names)
+    }
+    
+    mutating func removeMan() -> T?{
+        if people.count > 0 {
+            return people.removeFirst()
+        } else {
+            return nil
+        }
+    }
+    
+    func filter(array: [T], predicateSomeClosure: (T) -> Bool) -> [T]{
+        var tmpArray = [T]()
+        for element in array{
+            if predicateSomeClosure(element){
+                tmpArray.append(element)
+            }
+        }
+        return tmpArray
+    }  
+    subscript(index: Int) -> T?{
+        if index > people.count || index < 0{
+            return nil
+        }else{
+            return people[index]
+        }
+    }
+    
+}
+
+var queue = Queue<Int>()
+queue.addPeople(names: [1,2,3,4,5,6,7,8,9,10,11,12,13])
+print(queue)
+
+queue.addMan(name: 14)
+print(queue)
+
+
+let filteredQueue = queue.filter(array: queue.people) { (number: Int) -> Bool in
+    return number % 2 == 0
+}
+print(filteredQueue)
+
+print(queue[15])
